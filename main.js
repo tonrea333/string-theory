fc69a5cd664f3698928b3bf66b0bd33273683521
tonrea333 <readushartoniii@Readuss-MacBook-Pro.local>
2021-06-10 10:11:04 -0400

    all

---
+++ main.js
@@ -0,0 +1,82 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+//Prompt user for input 
+
+let valueEntered = prompt("Enter a word.")
+
+//User input is inserted into to function for processing
+
+function output(xify){
+
+
+    for (i = 0; i == valueEntered; i++){
+
+//If statement confirms string for action otherwise it is processed out through else statement
+
+    if (valueEntered === String){
+       const letterRemove = valueEntered.slice(valueEntered);
+        const addX = "x";
+        valueEntered.push("x");
+        console.log(xify);
+
+    }else {"User must enter a string word."}
+}
+    return xify;
+}
+
+//"returns the given string with an exclamation point after each character"
+
+function yellingChars(someString)
+
+for (i = 0; i + yellingChars; i++){
+    let exclamationEnter = "!";
+    yellingChars.splice(yellingChars.length -1, 1);
+
+    console.log(yellingChars);
+
+    return yellingChars;
+}
+
+//"adds the index of each character before that character in the given string"
+
+
+function indexedChars(characterIndex);{
+
+for (i = 0; i < 5; i++){
+indexedChars.slice(yellingChars[2,4,6,8,10,12]);
+indexedChars.push([0],[2],[4],[6],[8]);
+}
+return indexedChars;
+}
+
+//"adds the number of each character before that character in the given string"
+function numberedChars(addNumber);
+
+let text =[]
+for (const number of numberedChars ) {
+ text += number;
+ console.log(numberedChars);
+ return numberedChars;
+}
+
+//"returns the given sentence with every question mark or period changed to an exclamation point"
+
+function exclaim(){
+exclaim.shift = "?";
+exclaim.unshift = "!";
+
+console.log(exclaim);
+
+return exclaim;
+}
+
+//"returns the given string repeated n times, where n is the second parameter"
+
+function repeatIt(userEntry);
+
+let text = "";
+
+for (const repeatTimes of repeatIt){
+
+}
+
